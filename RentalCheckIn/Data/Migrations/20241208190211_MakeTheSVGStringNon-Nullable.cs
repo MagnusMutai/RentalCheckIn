cf9e864613119c9475e64de051df3313bf86a7f6
Magnus Mutai <MagnusMq07@gmail.com>
2024-12-08 23:47:06 +0300

    Removed the nullable property of Svg in Language table

---
+++ RentalCheckIn/Data/Migrations/20241208190211_MakeTheSVGStringNon-Nullable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentalCheckIn.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeTheSVGStringNonNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "language",
+                keyColumn: "svg",
+                keyValue: null,
+                column: "svg",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "svg",
+                table: "language",
+                type: "text",
+                nullable: false,
+                collation: "utf8mb3_general_ci",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldCollation: "utf8mb3_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("MySql:CharSet", "utf8mb3");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "svg",
+                table: "language",
+                type: "text",
+                nullable: true,
+                collation: "utf8mb3_general_ci",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "utf8mb3_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("MySql:CharSet", "utf8mb3");
+        }
+    }
+}
