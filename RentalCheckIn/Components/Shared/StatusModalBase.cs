d720ca0c0156fd47220e7f79e6976d5ad275e432
Magnus Mutai <MagnusMq07@gmail.com>
2024-12-15 10:39:37 +0300

    Added Abstracted CheckIn Modal popup to its own component called StatusModal

---
+++ RentalCheckIn/Components/Shared/StatusModalBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RentalCheckIn.Components.Shared;
+public class StatusModalBase : ComponentBase
+{
+    [Parameter]
+    public bool IsOpen { get; set; }
+    [Parameter]
+    public bool IsSuccess { get; set; }
+    [Parameter]
+    public string? Message {  get; set; }    
+    [Parameter]
+    public EventCallback OnClose { get; set; }
+
+    protected string ModalVisibility => IsOpen ? "show d-block" : "d-none";
+
+    protected async Task Close()
+    {
+        await OnClose.InvokeAsync();
+    }
+}
